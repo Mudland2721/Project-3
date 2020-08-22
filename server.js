0722dc2366b09b92f4e87ef1df7cd0000bc705aa
Aaron <aaronkh19@gmail.com>
2020-08-22 10:20:40 -0600

    setup server

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 const mongoose = require("mongoose");
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/songs", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});

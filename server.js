954744e093d4a56762f436d84b8d29695fc0f37b
Aaron <aaronkh19@gmail.com>
2020-09-03 19:35:11 -0600

    pushing what i have for the login authentication

--- server.js
+++ server.js
@@ -1,24 +1,35 @@
-// const mongoose = require("mongoose");
 const express = require("express");
-// const morgan = require("morgan");
-const routes = require("./routes");
+const expressLayouts = require("express-ejs-layouts");
+const mongo = require("mongodb");
+const mongoose = require("mongoose");
+const flash = require("connect-flash");
+const session = require("express-session");
+
+// const db = mongoose.connection;
 
 const app = express();
 
+// Connect to Mongo
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/songapp", {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+// EJS
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+
+// Bodyparser
+app.use(express.urlencoded({ extended: false }));
+
+// Express Session Middleware
+
+// Routes
+app.use("/", require("./routes/index"));
+app.use("/users", require("./routes/users"));
+
 const PORT = process.env.PORT || 3000;
 
-app.use(morgan("dev"));
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static("public"));
-app.use(routes);
-
-mongoose.connect("mongodb://localhost/songs", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-});
-
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
+app.listen(PORT, console.log(`Server started on port ${PORT}`));

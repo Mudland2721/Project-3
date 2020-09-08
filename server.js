5052b9ce6a2c53abdebdb1da28f1da5355e20bad
Aaron <aaronkh19@gmail.com>
2020-09-08 12:58:50 -0600

    deleted old login, started new one with react

--- server.js
+++ server.js
@@ -1,35 +1,59 @@
-const express = require("express");
-const expressLayouts = require("express-ejs-layouts");
-const mongo = require("mongodb");
 const mongoose = require("mongoose");
-const flash = require("connect-flash");
-const session = require("express-session");
-
-// const db = mongoose.connection;
+const express = require("express");
+const cors = require("cors");
+const passport = require("passport");
+const passportLocal = require("passport-local").Strategy;
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcryptjs");
+const expressSession = require("express-session");
+const bodyParser = require("body-parser");
+const User = require("./models/User");
 
 const app = express();
 
-// Connect to Mongo
+// Setting up Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+app.use(
+  expressSession({
+    secret: "secretCode",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use(cookieParser("secretcode"));
+
+// Routes
+app.post("/login", (req, res) => {
+  console.log(req.body);
+});
+
+app.post("/register", (req, res) => {
+  console.log(req.body);
+});
+
+app.get("/user", (req, res) => {
+  console.log(req.body);
+});
+
+// Connect to Mongoose
 mongoose
-  .connect(process.env.MONGODB_URI || "mongodb://localhost/songapp", {
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/loginapp", {
     useNewUrlParser: true,
   })
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
-// EJS
-app.use(expressLayouts);
-app.set("view engine", "ejs");
-
-// Bodyparser
-app.use(express.urlencoded({ extended: false }));
-
-// Express Session Middleware
-
-// Routes
-app.use("/", require("./routes/index"));
-app.use("/users", require("./routes/users"));
-
+// Setting up PORT
 const PORT = process.env.PORT || 3000;
 
+// Setting up Server
 app.listen(PORT, console.log(`Server started on port ${PORT}`));

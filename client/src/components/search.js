d23df3379fbd4afe1c7e879a977da2cabcb710ce
Aaron <aaronkh19@gmail.com>
2020-09-09 21:57:49 -0600

    updated search.js with api call

--- client/src/components/search.js
+++ client/src/components/search.js
@@ -1,43 +1,48 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import InputBase from "@material-ui/core/InputBase";
-import IconButton from "@material-ui/core/IconButton";
-import SearchIcon from "@material-ui/icons/Search";
+import React, { Component } from "react";
+import { Textfield } from "react-mdl";
 import axios from "axios";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    padding: "2px 4px",
-    display: "flex",
-    alignItems: "center",
-    width: 250,
-  },
-  input: {
-    marginLeft: theme.spacing(1),
-    flex: 1,
-  },
-  iconButton: {
-    padding: 10,
-  },
-}));
+class Search extends Component {
+  state = {
+    artist: "",
+  };
 
-export default function CustomizedInputBase() {
-  const classes = useStyles();
+  componentDidMount() {
+    this.fetchArtist();
+  }
 
-  return (
-    <Paper component='form' className={classes.root}>
-      <InputBase
-        className={classes.input}
-        placeholder='Search'
-        inputProps={{ "aria-label": "search google maps" }}
+  fetchArtist = () => {
+    axios({
+      method: "GET",
+      url: "https://deezerdevs-deezer.p.rapidapi.com/search",
+      headers: {
+        "content-type": "application/octet-stream",
+        "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+        "x-rapidapi-key": "d0b39321a7msha49b7aec705a8c7p18e760jsnf7d5b65d6c7c",
+        useQueryString: true,
+      },
+      params: {
+        q: "eminem",
+      },
+    })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  render() {
+    return (
+      <Textfield
+        onChange={() => {}}
+        label='Expandable Input'
+        expandable
+        expandableIcon='search'
       />
-      <IconButton
-        type='submit'
-        className={classes.iconButton}
-        aria-label='search'>
-        <SearchIcon />
-      </IconButton>
-    </Paper>
-  );
+    );
+  }
 }
+
+export default Search;

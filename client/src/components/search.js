ba5ab187966d058c97e023592a2342a701eb7dcc
Mason <Mudland2721@gamil.com>
2020-09-11 11:45:37 -0600

    working on search field

--- client/src/components/search.js
+++ client/src/components/search.js
@@ -7,38 +7,65 @@ class Search extends Component {
     artist: "",
   };
 
-  componentDidMount() {
-    this.fetchArtist();
-  }
+  // componentDidMount() {
+  //   document.addEventListener("keydown");
+  // }
 
-  fetchArtist = () => {
-    axios({
-      method: "GET",
-      url: "https://deezerdevs-deezer.p.rapidapi.com/search",
-      headers: {
-        "content-type": "application/octet-stream",
-        "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-        "x-rapidapi-key": "d0b39321a7msha49b7aec705a8c7p18e760jsnf7d5b65d6c7c",
-        useQueryString: true,
-      },
-      params: {
-        q: this.state.artist,
-      },
-    })
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+  // handleInputChange = event => {
+  //   set state
+  //   if user typed in more than 3 characters
+  //     wait a second
+  //     call the API
+  // };
+
+  // onKeyDown(e) {
+  //   // make api call here
+
+  //   if (e.key === `Enter`) {
+  //     console.log(`ENTER CLICKED!!!!!!!!!!!!!!!!!!!!!!!!!!!`);
+  //   }
+  // }
+
+  change = (e) => {
+    this.setState({
+      [e.target.value]: e.target.value,
+    });
   };
+
+  handleFormSubmit(event) {
+    event.preventDefault();
+    fetchArtist();
+
+    function fetchArtist() {
+      axios({
+        method: "GET",
+        url: "https://deezerdevs-deezer.p.rapidapi.com/search",
+        headers: {
+          "content-type": "application/octet-stream",
+          "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+          "x-rapidapi-key":
+            "d0b39321a7msha49b7aec705a8c7p18e760jsnf7d5b65d6c7c",
+          useQueryString: true,
+        },
+        params: {
+          q: this.state.artist,
+        },
+      })
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }
 
   render() {
     return (
       <Textfield
-        onChange={(e) => this.setState({ artist: e.target.value })}
+        onChange={(e) => this.change(e)}
+        placeholder='search'
         label='Expandable Input'
-        value={this.state.artist}
         expandable
         expandableIcon='search'
       />

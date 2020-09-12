42ed776431eb208165d9b82fdf5b8f11af3f4f42
Mason <Mudland2721@gamil.com>
2020-09-11 19:19:05 -0600

    data coming back correctly from axios

--- client/src/components/search.js
+++ client/src/components/search.js
@@ -13,50 +13,47 @@ class Search extends Component {
     //handle change
     this.keyPressed = this.keyPressed.bind(this);
     this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    // this.fetchArtist = this.fetchArtist.bind(this);
   }
 
   change = (e) => {
-    console.log("e.target.value:", e.target.value);
+    // console.log("e.target.value:", e.target.value);
     this.setState({
-      [e.target.value]: e.target.value,
-    });
-    this.setState({
-      [e.target.value]: "",
+      [e.target.name]: e.target.value,
     });
   };
 
   keyPressed(event) {
     if (event.key === "Enter") {
-      this.handleFormSubmit();
+      this.handleFormSubmit(event);
     }
   }
 
   handleFormSubmit(event) {
     event.preventDefault();
-    fetchArtist();
-
-    function fetchArtist() {
-      axios({
-        method: "GET",
-        url: "https://deezerdevs-deezer.p.rapidapi.com/search",
-        headers: {
-          "content-type": "application/octet-stream",
-          "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-          "x-rapidapi-key":
-            "d0b39321a7msha49b7aec705a8c7p18e760jsnf7d5b65d6c7c",
-          useQueryString: true,
-        },
-        params: {
-          q: this.state.artist,
-        },
+    console.log(
+      `HERE WE GOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO`
+    );
+    // console.log(this.state.name);
+    axios({
+      method: "GET",
+      url: "https://deezerdevs-deezer.p.rapidapi.com/search",
+      headers: {
+        "content-type": "application/octet-stream",
+        "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+        "x-rapidapi-key": "d0b39321a7msha49b7aec705a8c7p18e760jsnf7d5b65d6c7c",
+        useQueryString: true,
+      },
+      params: {
+        q: this.state.artist,
+      },
+    })
+      .then((data) => {
+        console.log(data);
       })
-        .then((data) => {
-          console.log(data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    }
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   render() {
@@ -66,6 +63,7 @@ class Search extends Component {
         onKeyPress={this.keyPressed}
         placeholder='Search'
         label='Expandable Input'
+        name='artist'
         expandable
         expandableIcon='search'
       />
